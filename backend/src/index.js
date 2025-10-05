59ecafd3b095b1dd7e23a33f46274e437884ecbf
jasleen <jasleen.matharu@outlook.com>
2025-10-05 03:41:11 -0700

    commit

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,12 +1,14 @@
 // backend/src/index.js
+const cors = require('cors');
 const dotenv = require("dotenv");
 const express = require("express");
 const mongoose = require("mongoose");
 
+
 dotenv.config();
 
 const app = express();
-// app.use(cors());
+app.use(cors());
 app.use(express.json());
 
 mongoose.connect(process.env.MONGO_URI)
